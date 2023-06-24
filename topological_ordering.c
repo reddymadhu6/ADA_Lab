a0d3551958b5cf153e07d78818d1c6ba246af061
Madhusoodan Reddy <98515208+reddymadhu6@users.noreply.github.com>
2023-06-24 12:16:11 +0530

    Update topological_ordering.c

--- topological_ordering.c
+++ topological_ordering.c
@@ -1,39 +1,64 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
 
-int visited[50],graph[10][10],n,stack[10],top=-1;
+const int MAX = 10;
+void fnTopological(int a[MAX][MAX], int n);
+int main(void)
+{
+    int a[MAX][MAX],n;
+    int i,j;
 
-void topological_sort(int node){
-    visited[node]=1;
+    printf("Topological Sorting Algorithm -\n");
+    printf("\nEnter the number of vertices : ");
+     scanf("%d",&n);
 
-    for(int j=0;j<n;j++){
-        if(graph[node][j]==1 && visited[j]!=1){
-            topological_sort(j);
-        }
-    }
+    printf("Enter the adjacency matrix -\n");
+    for (i=0; i<n; i++)
+        for (j=0; j<n; j++)
+            scanf("%d",&a[i][j]);
 
-    stack[++top]=node;
+    fnTopological(a,n);
+    printf("\n");
+    return 0;
 }
 
-int main(){
-printf("Enter Number of nodes\n");
-scanf("%d",&n);
-printf("Enter the matrix\n");
-for(int i=0;i<n;i++){
-    for(int j=0;j<n;j++){
-        int key;
-        scanf("%d",&key);
-        graph[i][j]=key;
-    }
-}for(int i=0;i<n;i++){
-        topological_sort(i);
-    }
+void fnTopological(int a[MAX][MAX], int n)
+{
+    int in[MAX], out[MAX], stack[MAX], top=-1;
+    int i,j,k=0;
 
-
-    printf("\nTOPOLOGICAL SORT\n");
-    while(top!=-1){
-        printf("%d ",stack[top--]);
+    for (i=0;i<n;i++)
+    {
+        in[i] = 0;
+        for (j=0; j<n; j++)
+            if (a[j][i] == 1)
+                in[i]++;
     }
 
-return 0;
+    while(1)
+    {
+        for (i=0;i<n;i++)
+        {
+            if (in[i] == 0)
+            {
+                stack[++top] = i;
+                in[i] = -1;
+            }
+        }
+
+        if (top == -1)
+            break;
+
+        out[k] = stack[top--];
+
+        for (i=0;i<n;i++)
+        {
+            if (a[out[k]][i] == 1)
+                in[i]--;
+        }
+        k++;
+    }
+
+    printf("Topological Sorting (JOB SEQUENCE) is:- \n");
+    for (i=0;i<k;i++)
+        printf("%d ",out[i] + 1);
 }
